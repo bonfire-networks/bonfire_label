838f74008c9817a0d6a5c6ef0a8faf8ed9079967
Mayel de Borniol <git@mayel.space>
2024-01-25 19:02:38 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/831

--- lib/web/components/label_widget/labelled_live.ex
+++ lib/web/components/label_widget/labelled_live.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.Label.Web.LabelledLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop labelled, :any, default: nil
+  prop label, :any, default: nil
   prop activity, :any, default: nil
   prop showing_within, :atom, default: nil
   prop class, :css_class, default: ["flex items-center -ml-6 justify-start pb-2 mb-2"]
