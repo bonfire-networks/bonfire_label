e2a821839a63ef3a3b41e479cf3831c4574dc48c
ivan <ivanminutillo@outlook.it>
2024-09-02 10:43:44 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/831

--- lib/web/components/label_widget/labelled_live.ex
+++ lib/web/components/label_widget/labelled_live.ex
@@ -5,5 +5,6 @@ defmodule Bonfire.Label.Web.LabelledLive do
   prop label_id, :any, default: nil
   prop activity, :any, default: nil
   prop showing_within, :atom, default: nil
+  prop viewing_main_object, :boolean, default: false
   prop class, :css_class, default: ["flex items-center -ml-6 justify-start pb-2 mb-2"]
 end
