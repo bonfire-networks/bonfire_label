defad715f295403f1b62ec2413fc27d20357b0a8
ivan <ivanminutillo@outlook.it>
2025-03-19 15:43:39 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1081

--- lib/web/components/modal/content_label_live.ex
+++ lib/web/components/modal/content_label_live.ex
@@ -4,6 +4,8 @@ defmodule Bonfire.Label.Web.ContentLabelLive do
   alias Bonfire.Label.ContentLabels
 
   prop object, :any, required: true
+  prop scope, :any, required: true
+  prop parent_id, :any, required: true
   prop object_boundary, :any, default: nil
   prop btn_label, :string, default: nil
   prop target, :any, default: nil
